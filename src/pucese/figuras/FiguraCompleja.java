d6f0823348f926886b69cff2d70e7e971ee4b349
jaimepsayago <jaime.sayago@gmail.com>
2020-12-08 11:54:40 -0500

    commit figuras complejas metodos estaticos

--- src/pucese/figuras/FiguraCompleja.java
+++ src/pucese/figuras/FiguraCompleja.java
@@ -1,9 +1,26 @@
 package pucese.figuras;
 
 public class FiguraCompleja {
-
+	public static final int Max_Value  = 4;
+	public static final int Min_Value  = 1;
+	public static final int Max_Value1  = 16;
 	public static void main(String[] args) {
-		for (int line= 1; line <=4; line++) {
+		printTop();
+		figuraSuperior();
+		figuraInferior();
+		printTop();
+ 
+	}
+	public static void printTop() {
+		System.out.print("#");
+		for (int i = 1; i<=Max_Value1; i++) {
+			System.out.print("=");
+		}
+		System.out.println("#");
+	}
+
+	public static void figuraSuperior() {
+		for (int line= 1; line <=Max_Value ; line++) {
 			System.out.print("|");	
 			for (int space = 1; space <= (line * -2+8);space++) {
 				System.out.print(" ");
@@ -18,7 +35,23 @@ public class FiguraCompleja {
 			}
 			System.out.println("|");
 		}
-
+	}
+	public static void figuraInferior() {
+		for (int line= 4; line >=Min_Value; line--) {
+			System.out.print("|");	
+			for (int space = 1; space <= (line * -2+8);space++) {
+				System.out.print(" ");
+			}	
+			System.out.print("<>");
+			for(int dot = 1; dot <=(line *4-4);dot ++) {
+				System.out.print(".");
+			}	
+			System.out.print("<>");
+			for(int space = 1;space <= (line *-2 +8);space++){
+				System.out.print(" ");
+			}
+			System.out.println("|");
+		}
 	}
 
 }
